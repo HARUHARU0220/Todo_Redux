2edb671ecb8d1784aa6b96a4cdfc154736691b36
HARU <chu_ba@naver.com>
2024-01-09 22:46:45 +0900

    feat: added styled-components , next/babel for SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { NextPage } from "next"; 
+import styled from "styled-components"; 
 
-const index = () => {
-    return <div>hello TypeScript</div>; 
+const Container = styled.div`
+    padding: 20px; 
+`;
+
+const index: NextPage = () => {
+    return (
+    <Container>
+        <h1>hello Styled-components</h1>
+        <h2>hello Styled-components</h2>
+        <p>Styled-components</p>
+        <ul><li>hello Styled-components</li></ul>
+        <a>hello Styled-components</a>
+        <span>hello Styled-components</span>
+        </Container>
+); 
 }; 
 
-export default index; 
+export default index;
